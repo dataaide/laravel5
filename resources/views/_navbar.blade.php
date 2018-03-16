4309b852f82d1ff122b298d9fb7d1d86535dae6b
ramamy <mramambasoa@gmail.com>
2018-03-16 21:35:46 +0100

    Modif sur le jumbotron

--- resources/views/_navbar.blade.php
+++ resources/views/_navbar.blade.php
@@ -21,46 +21,50 @@
 
 <!-- Jumbotron -->
 <div class="container padding-100">
-<div class="row">
-    <div class="col-md-4 mr-auto">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                <img class="d-block w-100" src="http://laravel55.local/images/chatons.jpeg" alt="First slide">
+    <div class="row">
+        <div class="col-md-4 mr-auto">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                    <img class="d-block w-100" src="http://laravel55.local/images/chatons.jpeg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                    <img class="d-block w-100" src="http://laravel55.local/images/chat1.jpeg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                    <img class="d-block w-100" src="http://laravel55.local/images/chat2.jpg" alt="Third slide">
+                    </div>
                 </div>
-                <div class="carousel-item">
-                <img class="d-block w-100" src="http://laravel55.local/images/chat1.jpeg" alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                <img class="d-block w-100" src="http://laravel55.local/images/chat2.jpg" alt="Third slide">
-                </div>
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
         </div>
-    </div>
 
-    <div class="col-md-8 ml-auto">
-        <div class="jumbotron">
-                <h1 class="display-4">Hello, world!</h1>
-                <p>
-                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam culpa perferendis, cum eius eaque similique commodi itaque, natus ipsum ullam ipsam cumque. Aliquam enim, accusamus nobis reprehenderit aperiam ipsam eligendi.
-                </p>
+        <div class="col-md-8 ml-auto">
+            <div class="jumbotron">
+                    <h1 class="display-4">Hello, world!</h1>
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam culpa perferendis, cum eius eaque similique commodi itaque, natus ipsum ullam ipsam cumque. Aliquam enim, accusamus nobis reprehenderit aperiam ipsam eligendi.
+                    </p>
+
+                    <p>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam culpa perferendis, cum eius eaque similique commodi itaque, natus ipsum ullam ipsam cumque. Aliquam enim, accusamus nobis reprehenderit aperiam ipsam eligendi.
+                    </p>
+            </div>
         </div>
+        
     </div>
-    
-</div>
 
 </div>
 
